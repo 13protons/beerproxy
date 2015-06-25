b5a8b47be77855a775497b82ffc676b62c5d2c0f
Alan Languirand <alan@13protons.com>
2015-06-24 20:48:15 -0400

    better 404

--- server.js
+++ server.js
@@ -2,11 +2,10 @@ var env = process.env.RACK_ENV;
 
 var express = require('express')
   , compression = require('compression')
-  , http = require('http')
   , pantry = require('redpantry')
   , app = express();
 
-pantry.configure({ shelfLife: 60 });
+pantry.configure({ shelfLife: (60 * 15) });
 
 app.use(compression()); //gzip!
 
@@ -14,21 +13,13 @@ app.get("/menu_widgets/:resource", function(req, res, next){
   var url = 'https://www.beermenus.com/menu_widgets/' + req.params.resource
   res.setHeader('Access-Control-Allow-Origin', '*');
 
-  // request(url, function (error, response, body) {
-  //   if (!error && response.statusCode == 200) {
-  //     res.send(body);
-  //   }
-  // })
-
   pantry.fetch({ uri: url}, function (error, item, contentType) {
     res.send(item);
   });
 
 })
 
-app.use(function(req, res, next){
-  res.end('love beer :)')
-})
+app.use(function(req, res, next){ res.sendfile('./public/index.html'); })
 
 var port = process.env.PORT || 8080;
 app.listen(port);
